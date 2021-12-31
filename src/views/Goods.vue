4ae385abcb74089f3ae8173fc80ad6c6bbc1a247
JeremyLee <1429042985@qq.com>
2021-12-31 13:45:28 +0800

    push

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -14,11 +14,75 @@
 <script>
 import LeftNav from "../components/LeftNav.vue";
 export default {
+  name: "Goods",
   components: {
     LeftNav,
+  },
+  data() {
+    return {
+      goodsForm: {
+        name: "",
+        id: "",
+        shopName: "",
+        status: "",
+        minCommission: "",
+        maxCommission: "",
+      },
+    };
   },
 };
 </script>
 
-<style>
+
+
+<style lang="less" scoped>
+.tac {
+  .el-menu-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+  .el-menu-vertical-demo {
+    height: calc(100vh - 61px);
+  }
+}
+.content {
+  overflow: hidden;
+  .board {
+    background: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    margin: 10px;
+    border-radius: 4px;
+    // height: calc(100vh - 81px);
+    .el-breadcrumb {
+      padding: 12px;
+    }
+    .el-divider {
+      margin: 0;
+    }
+    .goods-form {
+      margin: 20px 0 0 50px;
+      width: 1200px;
+      display: flex;
+      flex-wrap: wrap;
+
+      .goods-item {
+        margin-right: 10px;
+        .goods-input {
+          width: 200px;
+        }
+        .goods-limit {
+          width: 88px;
+        }
+      }
+    }
+    .divider {
+      padding: 0 50px;
+    }
+    .tabs {
+      margin: 0 50px;
+    }
+  }
+}
 </style>
