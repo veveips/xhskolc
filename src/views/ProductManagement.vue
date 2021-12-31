5677e20317b4be2d0e1b9e997b626593069d6e1c
谢梽 <553238045@qq.com>
2021-12-31 10:58:22 +0800

    Update ProductManagement.vue
    1231

--- src/views/ProductManagement.vue
+++ src/views/ProductManagement.vue
@@ -89,7 +89,91 @@
                 <el-tab-pane label="快手" name="kuaishou">快手</el-tab-pane>
                 <el-tab-pane tap-position="right" label="回收站" >回收站</el-tab-pane>
               </el-tabs>
-          
+            </div>
+            <div class="goods-table">
+              <div class="goods-table-action">
+                <el-checkbox >当前全选</el-checkbox>
+                <el-button>加入商品组</el-button>
+                <el-button>同步商品</el-button>
+                <span class="syncTime" >最近同步时间：</span>
+              </div>
+              <el-table
+                ref="multipleTable"
+                :data="goodsData"
+                tooltip-effect="dark"
+                style="width: 100%"
+                @selection-change="handleSelectionChange">
+                <el-table-column
+                  type="selection"
+                  width="55">
+                </el-table-column>
+                <el-table-column
+                  label="商品信息"
+                  width="300"
+                  prop="info"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="price"
+                  label="现售价"
+                >
+                </el-table-column>
+                <el-table-column
+                  prop="reduction"
+                  label="直降价"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="commissionRate"
+                  label="佣金率"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="serviceRate"
+                  label="服务费率"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="promotionInfo"
+                  label="推广信息"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="source"
+                  label="来源"
+                  width="120"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="activitiesInfo"
+                  label="活动信息"
+                  width="120"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="remarks"
+                  label="备注"
+                  width="120"
+                  show-overflow-tooltip>
+                </el-table-column>
+                <el-table-column
+                  prop="action"
+                  label="操作"
+                  class="column-actions"
+                >
+                  <!-- 这里抽象出组件 -->
+                  <template slot-scope="scope">
+                    <div class="actions">
+                      <div @click="handleEdit(scope.$index, scope.row)">关闭推广</div>
+                      <div class="actions-activated">复制链接</div>
+                      <div>编辑竞品机制</div>
+                      <div>上传手卡</div>
+                      <div class="actions-activated">查看详情</div>
+                      <div>备注</div>
+                    </div>
+                  </template>
+                </el-table-column>
+              </el-table>
             </div>
           </div>
 
@@ -101,7 +185,7 @@
 
 <script>
 export default {
-  name: "Home",
+  name: "ProductManagement",
   components: {},
   data() {
     return {
@@ -112,8 +196,65 @@ export default {
         status: '',
         minCommission: '',
         maxCommission: ''
+      },
+       goodsData: [{
+          info: 'sb',
+          price: '12',
+          reduction: '10',
+          commissionRate: '10%',
+          serviceRate: '10%',
+          promotionInfo: 'you is sb',
+          source: 'none',
+          activitiesInfo: 'infofinof',
+          remarks: '无',
+          action: ''
+        }, {
+          info: 'sb',
+          price: '12',
+          reduction: '10',
+          commissionRate: '10%',
+          serviceRate: '10%',
+          promotionInfo: 'you is sb',
+          source: 'none',
+          activitiesInfo: 'infofinof',
+          remarks: '无',
+          action: ''
+        },{
+          info: 'sb',
+          price: '12',
+          reduction: '10',
+          commissionRate: '10%',
+          serviceRate: '10%',
+          promotionInfo: 'you is sb',
+          source: 'none',
+          activitiesInfo: 'infofinof',
+          remarks: '无',
+          action: ''
+        },{
+          info: 'sb',
+          price: '12',
+          reduction: '10',
+          commissionRate: '10%',
+          serviceRate: '10%',
+          promotionInfo: 'you is sb',
+          source: 'none',
+          activitiesInfo: 'infofinof',
+          remarks: '无',
+          action: ''
+        },{
+          info: 'sb',
+          price: '12',
+          reduction: '10',
+          commissionRate: '10%',
+          serviceRate: '10%',
+          promotionInfo: 'you is sb',
+          source: 'none',
+          activitiesInfo: 'infofinof',
+          remarks: '无',
+          action: ''
+        },],
+        multipleSelection: []
       }
-    };
   },
 };
 </script>
@@ -165,6 +306,28 @@ export default {
     }
     .tabs {
       margin: 0 50px;
+    }
+    .goods-table {
+      margin: 0 50px;
+      .goods-table-action {
+        float: left;
+        .el-checkbox {
+          margin-right: 15px;
+        }
+        .syncTime {
+          margin-left: 15px;
+          color: #a8a8a8;
+          font-size: 14px;
+        }
+      }
+      .actions {
+        color: #a8a8a8;
+        font-size: 12px;
+        text-align: right;
+        .actions-activated {
+          color: #409EFF;
+        }
+      }
     }
   }
 }
